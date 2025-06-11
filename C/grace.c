dc916f58c65ff9e7bb5f9773d22b38b443ba7d16
jfremond <jfremond@student.42.fr>
2025-06-11 16:46:51 +0200

    :white_check_mark: Grace in C is done

--- C/grace.c
+++ C/grace.c
@@ -1,16 +1,10 @@
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdio.h>
 #include <fcntl.h>
+/*
+	This is a comment
+*/
+#define STR "#include <stdio.h>%1$c#include <fcntl.h>%1$c/*%1$c	This is a comment%1$c*/%1$c#define STR %2$c%3$s%2$c%1$c#define FILE open(%2$cGrace_kid.c%2$c, O_CREAT | O_WRONLY | O_TRUNC, 0600)%1$c#define MAIN int main() {dprintf(FILE, STR, 10, 34, STR);}%1$c%1$cMAIN;%1$c"
+#define FILE open("Grace_kid.c", O_CREAT | O_WRONLY | O_TRUNC, 0600)
+#define MAIN int main() {dprintf(FILE, STR, 10, 34, STR);}
 
-# define MAIN main(){}
-int	main()
-{
-	char *s = "caca";
-	int file = open("Grace_kid.c", O_CREAT | O_RDWR);
-	if (file == -1)
-		return(-1);
-	else
-		write(file, s, strlen(s));
-}
+MAIN;
