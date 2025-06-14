f61457352541a1e3f6acfa4ef20406a63cd2273b
jfremond <jfremond@student.42.fr>
2025-06-14 15:32:00 +0200

    :white_check_mark: Colleen done

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,31 +1,26 @@
-; RDI - 1st argument
-; RSI - 2nd argument
-; RDX - 3rd argument
-; RCX - 4th argument
-; R8 - 5th argument
-; R9 - 6th argument
-; Stack - 7th+ arguments (pushed right-to-left)
-
 global	main
+; Calling external function printf
 extern	printf
+
 section	.text
-print: push rbx
-	mov	rdi, s
-	mov rsi, s
-	mov	rdx, 10
-	mov	rcx, 34
-	call	printf
-	pop rbx
-	ret
-main: push	rbx
-	call	print
-	pop	rbx
-	ret
+	print:
+		push	rbp
+		mov	rbp, rsp
+		mov	rdi, s
+		mov	rsi, s
+		mov	rdx, 10
+		mov	rcx, 34
+		call	printf
+		pop	rbp
+		ret
+	main:
+		; Stack alignment before calling the function
+		push	rbp
+		mov	rbp, rsp
+		call	print
+		pop	rbp
+		mov	rax, 0
+		ret
 
 section	.data
-	s	db "global	main%2$cextern	printf%2$csection	.data%2$c	quote	db 34%2$csection	.text%2$cprint:%2$c	lea	rdi, [rel s]%2$c	movzx	rsi, byte [quote]%2$c	mov	rax, 0%2$c	call	printf%2$c	mov	rax, 0%2$c	ret%2$cmain:%2$c	call	print%2$c	ret%2$csection .data%2$cs db %3$c%1$s%3$c%2$c"
-	; s: db "global	main%2$csection .data%2$ccode: db %3$c%1$s%3$c%2$c"
-	; nl	db 10
-	; tab	db 9
-	; quote	db 34
-	; code: db "; This is my first quine in ASM%2$cglobal _main%2$cextern _printf%2$csection .text%2$cprint:push rbx%2$cmov rdi, code%2$cmov rsi, code%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall _printf%2$cpop rbx%2$cret%2$c_main:push rbx ;always allign the stack to 16%2$ccall print%2$cpop rbx%2$cret%2$csection .data%2$ccode: db %3$c%1$s%3$c%2$c"
+	s	db "global	main%2$c; Calling external function printf%2$cextern	printf%2$c%2$csection	.text%2$c	print:%2$c		push	rbp%2$c		mov	rbp, rsp%2$c		mov	rdi, s%2$c		mov	rsi, s%2$c		mov	rdx, 10%2$c		mov	rcx, 34%2$c		call	printf%2$c		pop	rbp%2$c		ret%2$c	main:%2$c		; Stack alignment before calling the function%2$c		push	rbp%2$c		mov	rbp, rsp%2$c		call	print%2$c		pop	rbp%2$c		mov	rax, 0%2$c		ret%2$c%2$csection	.data%2$c	s	db %3$c%1$s%3$c%2$c"
