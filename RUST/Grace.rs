ffd758a19493ea2ffd5c50ead433b6a4a89c747b
jfremond <jfremond@student.42.fr>
2025-07-11 14:41:32 +0200

    :white_check_mark: Finished Grace in rust

--- RUST/Grace.rs
+++ RUST/Grace.rs
@@ -1,52 +1,29 @@
-/*
- *	This is a comment
- */
-// macro_rules! caca {
-// 	() => {
-// 		println!("caca");
-// 	};
-// }
-
-// macro_rules! caca1 {
-// 	($val:expr) => {
-// 		println!("{}", $val);
-// 	};
-// }
-
-
-// fn	main() {
-// 	caca!();
-// 	caca1!("pipi");
-// }
-
 use	std::fs::File;
 use	std::io::Write;
+
 /*
  *	Using macros here
  */
-macro_rules!	open_file {
-	() => {
+
+macro_rules!	open_and_write_in_file {
+	($val:expr) => {
 		let	f = File::create("Grace_kid.rs");
-		// let	file_content = b"Grace_kid.rs and caca";
-		// let	_ = f.expect("Error").write(file_content);
-		return f;
+		let	_ = f.expect("Error").write($val.as_bytes());
 	};
 }
 
-macro_rules!	write_file {
-	($val:expr) => {
-		let	s = b"ceci est un test";
-		let	_ = $val.expect("Error").write(s);
-	}
+macro_rules!	string_to_write {
+	($data:expr) => {
+		format!("use\tstd::fs::File;\nuse\tstd::io::Write;\n\n/*\n *\tUsing macros here\n */\n\nmacro_rules!\topen_and_write_in_file {{\n\t($val:expr) => {{\n\t\tlet\tf = File::create(\"Grace_kid.rs\");\n\t\tlet\t_ = f.expect(\"Error\").write($val.as_bytes());\n\t}};\n}}\n\nmacro_rules!\tstring_to_write {{\n\t($data:expr) => {{\n\t\tformat!({0:?}, $data)\n\t}};\n}}\n\nmacro_rules!\texec {{\n\t() => {{\n\t\tfn\tmain() {{\n\t\t\topen_and_write_in_file!(string_to_write!({0:?}));\n\t\t}}\n\t}};\n}}\n\nexec!();\n", $data)
+	};
 }
 
 macro_rules!	exec {
 	() => {
 		fn	main() {
-			open_file!();
-			write_file!();
+			open_and_write_in_file!(string_to_write!("use\tstd::fs::File;\nuse\tstd::io::Write;\n\n/*\n *\tUsing macros here\n */\n\nmacro_rules!\topen_and_write_in_file {{\n\t($val:expr) => {{\n\t\tlet\tf = File::create(\"Grace_kid.rs\");\n\t\tlet\t_ = f.expect(\"Error\").write($val.as_bytes());\n\t}};\n}}\n\nmacro_rules!\tstring_to_write {{\n\t($data:expr) => {{\n\t\tformat!({0:?}, $data)\n\t}};\n}}\n\nmacro_rules!\texec {{\n\t() => {{\n\t\tfn\tmain() {{\n\t\t\topen_and_write_in_file!(string_to_write!({0:?}));\n\t\t}}\n\t}};\n}}\n\nexec!();\n"));
 		}
-	}
+	};
 }
 
 exec!();
